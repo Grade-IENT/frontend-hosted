3560d2e394328b1eb8a9aa9cde69dfc32210cfa9
parikhro <100094364+parikhro@users.noreply.github.com>
2025-04-15 20:58:06 -0400

    fix typo

--- course_scheduler.py
+++ course_scheduler.py
@@ -91,7 +91,7 @@ def load_catalog(csv_path: Path) -> None:
     prereq_of = cat["Prereq"].to_dict()
 
 # AP Credit Handler Functions
-ap_df = pd.read_csv("../"+AP_CREDIT_CSV, dtype=str)
+ap_df = pd.read_csv("./"+AP_CREDIT_CSV, dtype=str)
 ap_df["Score"] = pd.to_numeric(ap_df["Score"], errors="coerce")
 
 def ap_fulfilled(ap_scores: Dict[str, int]) -> set[str]:
